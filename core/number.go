e6d82247c0d201829c22516c2ee81c2188ed778c
Antoine Gelloz <antoine.gelloz@me.com>
2022-11-03 16:55:04 +0100

    NewNumber

--- core/number.go
+++ core/number.go
@@ -1,13 +1,9 @@
 package core
 
-import (
-	"math/big"
-)
-
 type Number = MonetaryInt
 
 func NewNumber(i int64) *Number {
-	return (*Number)(big.NewInt(i))
+	return NewMonetaryInt(i)
 }
 
 func ParseNumber(s string) (*Number, error) {
