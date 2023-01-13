3a2c3496630fcffb598d58e82abb31da9b212072
Antoine Gelloz <antoine.gelloz@me.com>
2023-01-13 16:12:14 +0100

    first

--- core/account.go
+++ core/account.go
@@ -17,6 +17,10 @@ func (a Account) String() string {
 }
 
 func ParseAccount(acc Account) error {
+	// TODO: handle properly in ledger v1.10
+	if acc == "" {
+		return nil
+	}
 	if !accountRegexp.MatchString(string(acc)) {
 		return fmt.Errorf("accounts should respect pattern %s", accountPattern)
 	}
--- vm/machine_test.go
+++ vm/machine_test.go
@@ -1380,6 +1380,27 @@ func TestVariablesParsing(t *testing.T) {
 		}))
 	})
 
+	// TODO: handle properly in ledger v1.10
+	t.Run("account empty string", func(t *testing.T) {
+		p, err := compiler.Compile(`
+			vars {
+				account $acc
+			}
+			set_tx_meta("account", $acc)
+		`)
+		require.NoError(t, err)
+
+		m := NewMachine(*p)
+
+		require.NoError(t, m.SetVars(map[string]core.Value{
+			"acc": core.Account(""),
+		}))
+
+		require.NoError(t, m.SetVarsFromJSON(map[string]json.RawMessage{
+			"acc": json.RawMessage(`""`),
+		}))
+	})
+
 	t.Run("monetary", func(t *testing.T) {
 		p, err := compiler.Compile(`
 			vars {
